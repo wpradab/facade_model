3153b14b078e749026b0726fd8d9dd25520059c8
Mathman123-cloud <wpradab@ucentral.edu.co>
2025-08-25 20:26:30 -0500

    update repo

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+"""
+Facade Model Library
+Author: William Prada
+Description:
+    Tools to process facade images:
+    - Remove objects using YOLO + LaMa inpainting.
+    - Detect and crop the main facade of a house.
+"""
+
+from .house_selector import find_house_in_image
+from .object_removal import remove_objects_from_image
+
+__all__ = [
+    "find_house_in_image",
+    "remove_objects_from_image",
+]
